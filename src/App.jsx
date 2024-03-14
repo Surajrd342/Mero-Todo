0a9373744252f040c01d94a0d063d5ee09b678e7
Suraj Rd <surajrd342@gmail.com>
2024-03-14 23:32:35 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center content-center bg-slate-300 min-h-screen w-full'>
+      <h1 className="text-3xl font-bold text-red-500 underline bg-orange-300 ">
+        Hello world!
+      </h1>
+    </div>
+
+  )
+}
+
+export default App;
